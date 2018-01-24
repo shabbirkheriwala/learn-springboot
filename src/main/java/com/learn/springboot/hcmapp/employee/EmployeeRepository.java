6547bca17703ad6b2620c6d9a0408f20a8ea2755
Shabbir Kherywala <skherywala@deloitte.com>
2018-01-24 13:34:07 +0530

    Comments added

--- src/main/java/com/learn/springboot/hcmapp/employee/EmployeeRepository.java
+++ src/main/java/com/learn/springboot/hcmapp/employee/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.learn.springboot.hcmapp.employee;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
+    /*
+    * As a developer you write your repository interfaces,
+    * including custom finder methods,
+    * and Spring will provide the implementation automatically.
+    * */
 }
